59055832c171dccbf6e8f176ea8e8f504f2c4ba7
ariyanabdullah <ak01859136696@gmail.com>
2022-10-12 00:22:01 +0600

    second box style added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,7 +8,7 @@ const Blog = () => {
                <p className='text-md  font-semibold'>= React Router is a standard library for routing in React. <br/> It enables the navigation among views of various components in a React Application,<br/>  allows changing the browser URL, and keeps the UI in sync with the URL.</p>
             </div>
           {/* second box */}
-            <div>
+            <div  className='w-[60%] px-5 py-9 mx-auto shadow-xl rounded-lg my-7'>
                <h1 className='text-3xl font-semibold my-5'> 2. How does Context API works ?</h1> 
                <p className='text-md  font-semibold'>
                 =React.createContext() is all you need.  It returns a consumer and a provider. <br/> Provider is a component that as it's names suggests provides the state to its children.<br/>  It will hold the "store" and be the parent of all the components that might need that store.<br/>  Consumer as it so happens is a component that consumes and uses the state.
