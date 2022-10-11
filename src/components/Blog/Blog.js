bfc24522560430918ee901a48af8fb8d485cbf17
ariyanabdullah <ak01859136696@gmail.com>
2022-10-12 00:11:31 +0600

    second question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,6 +7,16 @@ const Blog = () => {
                <h1 className='text-3xl font-semibold my-5'> 1. What is the purpose of React Router?</h1> 
                <p className='text-md  font-semibold'>= React Router is a standard library for routing in React. <br/> It enables the navigation among views of various components in a React Application,<br/>  allows changing the browser URL, and keeps the UI in sync with the URL.</p>
             </div>
+          {/* second box */}
+            <div>
+               <h1 > 2. How does Context API works ?</h1> 
+               
+            </div>
+
+
+
+
+            
         </div>
     );
 };
